dc6a9c724a0aae59fb69023ce94ccacc8557ea0a
Radoslaw Wrobel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-03-13 14:05:40 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.InputMismatchException;
+
+public class Main
+  {
+  public static void main(String[] args)
+    {
+        for (int i = 0; i < 100; i++) {
+          if (i % 2 == 0) {
+            System.out.println("Liczba " + i + " jest parzysta");
+          }
+        }
+      }
+    }
